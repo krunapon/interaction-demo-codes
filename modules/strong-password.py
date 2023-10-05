04cfbade02a955a8e0d2a4d1b96429d785a09d86
Kanda Saikaew <krunapon@kku.ac.th>
2023-10-05 10:25:34 +0700

    update demo codes

--- modules/strong-password.py
+++ modules/strong-password.py
@@ -1,10 +1,13 @@
 import random
 import string
-pwd1 = random.choice(string.ascii_uppercase)
-pwd2 = random.choice(string.ascii_lowercase)
-pwd3 = random.choice(string.digits)
-pwd4 = random.choice(string.punctuation)
+
+upper = random.choice(string.ascii_uppercase)
+lower = random.choice(string.ascii_lowercase)
+digit = random.choice(string.digits)
+punct = random.choice(string.punctuation)
 characters = string.ascii_letters + string.digits + string.punctuation
-pwd5 = ''.join(random.choice(characters) for i in range(5,10))
-password = ''.join(pwd1 + pwd2 + pwd3 + pwd4 + pwd5)
+chars = ''.join(random.choice(characters) for i in range(5,10))
+password = ''.join(upper + lower + digit + punct + chars)
+print("Random password is:", password)
+password = ''.join(random.sample(password, len(password)))
 print("Random password is:", password)
